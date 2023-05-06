c178c15bf8907df1d65489874b1975f017b9b600
Thomas Noel <thomas.c.noel@gmail.com>
2023-05-06 15:49:31 -0700

    Add third player that uses puppies for ammunition

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ let player2 = new Player("ss", "blue");
 player2.pose.angle = -2
 player2.pose.x = 10
 
+let player3 = new Player("tn", "orange");
+player3.pose.angle= 3
+player.pose.x = 30
+
 engine.add_entity(player);
 engine.add_entity(player2);
+engine.add_entity(player3);
 
 let viewport = new Viewport(engine, "game-viewport", 800, 600);
 engine.add_viewport(viewport);
@@ -22,6 +27,8 @@ setInterval(() => {
     player.pose.turn(0.01)
     player2.pose.step_forward(1)
     player2.pose.turn(0.01)
+    player3.pose.step_forward(1)
+    player3.pose.turn(0.01)
 
     engine.update();
     viewport.render();
@@ -46,5 +53,32 @@ setInterval(()=>{
         if (!bullet.in_viewport(viewport)) 
             engine.remove_entity(bullet)
     });
+    
+}, 1000);
 
-}, 1000)
+setInterval(()=>{
+    let bullet = new PhysicsEntity("bullet2.png");
+    let bullet2 = new PhysicsEntity("bullet2.png");
+    let bullet3 = new PhysicsEntity("bullet2.png");
+   
+    let bullets = [bullet, bullet2, bullet3];
+    let offsets = [0, -Math.PI/6, Math.PI/6];
+
+    for (let i=0; i < 3; i++) {
+        bullets[i].pose.x = player3.pose.x;
+        bullets[i].pose.y = player3.pose.y;
+        bullets[i].pose.angle = player3.pose.angle + offsets[i];
+
+        //Calculate velocity
+        bullets[i].velocity.x = 10 * Math.sin(bullets[i].pose.angle);
+        bullets[i].velocity.y = 10 * -Math.cos(bullets[i].pose.angle);
+
+        engine.add_entity(bullets[i]);
+
+        bullets[i].pose.on('move', (pose) => {
+            if (!bullets[i].in_viewport(viewport))
+                engine.remove_entity(bullets[i])    
+        });
+    }
+
+}, 1000);
