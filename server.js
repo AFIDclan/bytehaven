98ef0c33c1814d477953a5ce59e83f981f3d4770
AFIDclan <afidclan@gmail.com>
2023-05-17 18:21:30 -0700

    Cleanup

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const fs = require("fs");
 
 const config = require("./config.json");
 
-const Game = require("./Game.js");
+const Game = require("./lib/Game.js");
 
 const log = Logger.console(config.log.namespace || "tasking-manager", config.log);
 log.set_log_level(config.log.level || "debug");
